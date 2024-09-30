ac766ee034d1ed311f85d724e887bd246870af0b
Meghan Balk <meghan.balk@gmail.com>
2024-09-30 19:39:18 +0200

    Update script.js

--- webpic/scripts/script.js
+++ webpic/scripts/script.js
@@ -3,7 +3,7 @@
  */ 
 
 var imagesURL = "https://meghalithic.github.io/megbalk.github.io/webpic/images/";
-
+// a comment
 var canvas = new fabric.Canvas('canvas', {selection: false, hoverCursor:'default'});
 var images;
 var bgimg;
